05c1056cdcafd9af5586da8fc222f7c203fc0558
maxjwebber <tua83483@temple.edu>
2020-02-17 16:24:04 -0500

    Intent and color changing behavior for Canvas Activity implemented

---
+++ app/src/main/java/edu/temple/paletteandcanvas/CanvasActivity.java
@@ -0,0 +1,24 @@
+package edu.temple.paletteandcanvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+        String myColor = getIntent().getStringExtra("myColor");
+        findViewById(R.id.layout2).setBackgroundColor(Color.parseColor(myColor));
+        TextView colorDisplay = findViewById(R.id.colorDisplay);
+        colorDisplay.setText(myColor);
+
+    }
+}
--- app/src/main/java/edu/temple/paletteandcanvas/PaletteActivity.java
+++ app/src/main/java/edu/temple/paletteandcanvas/PaletteActivity.java
@@ -2,11 +2,17 @@ package edu.temple.paletteandcanvas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public class PaletteActivity extends AppCompatActivity {
 
@@ -14,6 +20,7 @@ public class PaletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context = this;
         final String[] colors = getResources().getStringArray(R.array.colors);
         ColorAdapter colorAdapter = new ColorAdapter(this,colors);
         Spinner spinner = findViewById(R.id.spinner);
@@ -21,16 +28,15 @@ public class PaletteActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (position>0)
-                    findViewById(R.id.layout).setBackgroundColor(Color.parseColor(colors[position]));
-                else
-                    findViewById(R.id.layout).setBackgroundColor(Color.WHITE);
+                if (position>0) {
+                    Intent intent = new Intent(context, CanvasActivity.class);
+                    intent.putExtra("myColor",colors[position]);
+                    context.startActivity(intent);
+                }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                findViewById(R.id.layout).setBackgroundColor(Color.WHITE);
-            }
+            public void onNothingSelected(AdapterView<?> parent) { }
         });
     }
 }
