4a374e9c6921f3a3ac55affe1a5c9721636ea09e
maxjwebber <tua83483@temple.edu>
2020-02-17 15:14:32 -0500

    Initial commit. imported ColorAdapter from ColorActivity

---
+++ app/src/main/java/edu/temple/palleteandcanvas/ColorAdapter.java
@@ -0,0 +1,54 @@
+package edu.temple.coloractivity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class ColorAdapter extends BaseAdapter
+{
+    boolean isDropped;
+    private Context context;
+    private String[] colors;
+
+    public ColorAdapter(Context context, String[] colors)
+    {
+        this.context=context;
+        this.colors=colors;
+        this.isDropped = false;
+    }
+
+    @Override
+    public int getCount() {
+        return colors.length;
+    }
+
+    @Override
+    public String getItem(int position)
+    {
+        return colors[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public TextView getView(int position, View convertView, ViewGroup parent)
+    {
+        TextView textView = new TextView(context);
+        textView.setText(colors[position]);
+        return textView;
+    }
+
+    @Override
+    public TextView getDropDownView(int position, View convertView, ViewGroup parent) {
+        TextView textView = getView(position,convertView,parent);
+        if (position>0)
+            textView.setBackgroundColor(Color.parseColor(colors[position]));
+        return textView;
+    }
+}
---
+++ app/src/main/java/edu/temple/palleteandcanvas/PalleteActivity.java
@@ -0,0 +1,36 @@
+package edu.temple.palleteandcanvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+public class PalleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final String[] colors = getResources().getStringArray(R.array.colors);
+        ColorAdapter colorAdapter = new ColorAdapter(this,colors);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(colorAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position>0)
+                    findViewById(R.id.layout).setBackgroundColor(Color.parseColor(colors[position]));
+                else
+                    findViewById(R.id.layout).setBackgroundColor(Color.WHITE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                findViewById(R.id.layout).setBackgroundColor(Color.WHITE);
+            }
+        });
+    }
+}
